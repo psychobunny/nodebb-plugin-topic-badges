fa877c07112a3f0fe0775efd9674c870a3fcfa25
psychobunny <psycho.bunny@hotmail.com>
2014-02-21 17:12:53 -0500

    added button in thread tools to "mark as Solved"

--- library.js
+++ library.js
@@ -1,11 +1,36 @@
 (function(module) {
 	"use strict";
 
-	var TopicBadges = {}
+	var TopicBadges = {};
+
+	var SocketAdmin = module.parent.require('./socket.io/admin'),
+        Sockets = module.parent.require('./socket.io/index'),
+        Topics = module.parent.require('./topics.js');
+
+    TopicBadges.init = function() {
+    	SocketAdmin.topics.renameTopic = function(socket, data, callback) {
+    		console.log(data);
+    		if (!data.tid || !data.title) {
+    			return callback(false);
+    		}
+
+    		Topics.setTopicField(data.tid, 'title', data.title, callback);
+    	};
+    };
 
 	TopicBadges.addScripts = function(scripts, callback) {
 		scripts.push('plugins/nodebb-plugin-topic-badges/lib/main.js');
 		return scripts;
+	};
+
+	TopicBadges.addThreadTools = function(threadTools, callback) {
+		threadTools.push({
+			"title": "Mark <strong>Solved</strong>",
+			"class": "mark-solved",
+			"icon": "fa-check"
+		});
+
+		callback(null, threadTools);
 	};
 
 	module.exports = TopicBadges;
--- public/lib/main.js
+++ public/lib/main.js
@@ -13,10 +13,34 @@
 		});
 	}
 
+	function badgifyTitle(title, badge) {
+		if (title.match(/^\[.+\]/)) {
+			title = title.replace(/^\[.+\]/, '[' + badge + ']');
+		} else {
+			title = '[' + badge + '] ' + title;
+		}
+
+		return title;
+	}
+
 	jQuery('document').ready(function() {
 		$(window).on('action:ajaxify.end', function(ev, data) {
 			if (data.url.match(/^category/) || data.url.match(/^unread/) || data.url.match(/^recent/) || data.url.match(/^popular/)) {
 				badgifyTopics();
+			}
+
+			if (data.url.match(/^topic/)) {
+				$('.thread-tools .mark-solved').on('click', function() {
+					var title = badgifyTitle(templates.get('topic_name'), 'Solved');
+					socket.emit('admin.topics.renameTopic', {
+						tid: templates.get('topic_id'),
+						title: title
+					}, function(err) {
+						if (!err) {
+							$('.topic-title').html(title);
+						}
+					});
+				});
 			}
 		});
 
