1f67300fa05dfefc048d299ce84e819de7dc6072
psychobunny <psycho.bunny@hotmail.com>
2014-02-21 16:09:35 -0500

    match badges only at start of title; add badge on new topic; add badges to unread/recent/popular

--- public/lib/main.js
+++ public/lib/main.js
@@ -5,7 +5,7 @@
 		$('.category-item .topic-title').each(function() {
 			var el = $(this), match;
 
-			if (match = el.html().match(/\[.+\]/)) {
+			if (match = el.html().match(/^\[.+\]/)) {
 				el.html(match[0]
 					.replace('[', '<div class="badge topic-badge pull-right">')
 					.replace(']', '</div>') + el.html().replace(match[0], ''));
@@ -15,13 +15,12 @@
 
 	jQuery('document').ready(function() {
 		$(window).on('action:ajaxify.end', function(ev, data) {
-			if (data.url.match(/^category/)) {
+			if (data.url.match(/^category/) || data.url.match(/^unread/) || data.url.match(/^recent/) || data.url.match(/^popular/)) {
 				badgifyTopics();
 			}
 		});
 
-		$(window).on('action:categories.loaded', function() {
-			badgifyTopics();
-		});
+		$(window).on('action:categories.loaded', badgifyTopics);
+		$(window).on('action:categories.new_topic.loaded', badgifyTopics);
 	});
 }());
