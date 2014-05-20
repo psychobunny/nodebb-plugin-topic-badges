54560c697482c1ec6815a5ef99e4bf8f76d5fca6
Julian Lam <julian@designcreateplay.com>
2014-05-20 12:52:15 -0400

    updating plugin to remove use of staticDir in favour of staticDirs

--- library.js
+++ library.js
@@ -4,23 +4,23 @@
 	var TopicBadges = {};
 
 	var SocketAdmin = module.parent.require('./socket.io/admin'),
-        Sockets = module.parent.require('./socket.io/index'),
-        Topics = module.parent.require('./topics.js');
+		Sockets = module.parent.require('./socket.io/index'),
+		Topics = module.parent.require('./topics.js');
 
-    TopicBadges.init = function() {
-    	SocketAdmin.topics = SocketAdmin.topics || {};
-    	SocketAdmin.topics.renameTopic = function(socket, data, callback) {
-    		if (!data.tid || !data.title) {
-    			return callback(false);
-    		}
+	TopicBadges.init = function() {
+		SocketAdmin.topics = SocketAdmin.topics || {};
+		SocketAdmin.topics.renameTopic = function(socket, data, callback) {
+			if (!data.tid || !data.title) {
+				return callback(false);
+			}
 
-    		Topics.setTopicField(data.tid, 'title', data.title, callback);
-    	};
-    };
+			Topics.setTopicField(data.tid, 'title', data.title, callback);
+		};
+	};
 
 	TopicBadges.addScripts = function(scripts, callback) {
 		scripts.push('plugins/nodebb-plugin-topic-badges/lib/main.js');
-		return scripts;
+		callback(null, scripts);
 	};
 
 	TopicBadges.addThreadTools = function(threadTools, callback) {
